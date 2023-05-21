2708d13e7a32be3fe237b0df7c5cbd3521b5ac55
Nick Whaley <nicholasrwhaley@gmail.com>
2023-05-20 23:16:27 -0700

    added main component with button

---
+++ igt/app/main.jsx
@@ -0,0 +1,17 @@
+function MyButton() {
+    return (
+      <button>
+        Press here
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
